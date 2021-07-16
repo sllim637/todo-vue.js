67108248d211310a0f3b3faac802a4d82df03ced
sllim637 <slim.hammami1977@gmail.com>
2021-07-16 11:45:31 +0200

    adding carousel

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
-   <todos></todos>
-   <todos></todos>
+    <carousel>
+      <carousel-slide>
+      Salut les gens
+      <img src="https://picsum.photos/200/300?grayscale" alt="images">
+      </carousel-slide>
+      <carousel-slide>
+      aurevoir les gens
+      </carousel-slide>
+    </carousel>
    <todos></todos>
   </div>
 </template>
 
 <script>
 import Todos from './components/Todos.vue'
-
+import Carousel from './components/carousel/carousel.vue'
+import CarouselSlide from './components/carousel/CarouselSlide.vue'
 export default {
   name: 'App',
   components: {
-    Todos
+    Todos,
+    Carousel,
+    CarouselSlide
   }
 }
 </script>
---
+++ src/components/carousel/carousel.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="carousel">
+        <slot></slot>
+        <button class="carousel__nav carousel__next" @click.prevent="next"></button>
+        <button class="carousel__nav carousel__prev" @click.prevent="prev"></button>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    console.log(this.$children)
+    return {
+      index: 0,
+      slides: []
+    }
+  },
+  mounted () {
+    this.slides = this.$children
+    this.slides.forEach((slide, i) => {
+      slide.index = i
+    })
+  },
+  methods: {
+    next () {
+      this.index++
+      if (this.index === this.slidesCount) {
+        this.index = 0
+      }
+    },
+    prev () {
+      this.index--
+      if (this.index < 0) {
+        this.index = this.slidesCount - 1
+      }
+    }
+  },
+  computed: {
+    slidesCount () {
+      return this.slides.length
+    }
+  }
+}
+</script>
+<style>
+.carousel {
+  position: relative;
+}
+.carousel__nav {
+  position: absolute;
+  top: 50% ;
+  left: 10px;
+  background: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fnl%2Fsearch%2Fnext%2Bbutton&psig=AOvVaw3fd5uN2lV06bQ1SAfctKit&ust=1626423479167000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCIC60-DR5PECFQAAAAAdAAAAABAJ');
+  width: 63px;
+  height: 63px;
+}
+</style>
